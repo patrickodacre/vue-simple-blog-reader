869036c87ad848fc9e7cf098a54371fe5b508f55
patrickodacre <patrick@patrickwho.me>
2016-12-12 06:25:01 -0500

    Fix ajax errors, move detailed explanation to blog to keep code more readable.

--- main.js
+++ main.js
@@ -1,27 +1,38 @@
 new Vue({
-  name: 'MainApp',
-  el: '#app',
-  data: {
-    posts: [],
-    selectedPost: {
-      title: 'Select a Post',
-      content: 'Once you select a post on the left, the content will display here.'
-    }
-  },
-  created() {
-    this.$http.get('https://public-api.wordpress.com/rest/v1.1/sites/stearns.wordpress.com/posts/')
-      .then((resp) => {
+    name   : 'MainApp', // naming your Vue components and instances helps with debugging. Get in the habit.
+    el     : '#app', // we're mounting this instance to the ID app
+    data   : {
+        posts       : [],
+        selectedPost: {
+            title  : 'Select a Post',
+            content: 'Once you select a post on the left, the content will display here.'
+        }
+    },
+    // Step 1: Life cycle hook, 'created' fires
+    created() {
 
-        this.posts = resp.body.posts
-      })
-  },
-  methods: {
-    selectPost(postID) {
-      this.$http.get('https://public-api.wordpress.com/rest/v1.1/sites/stearns.wordpress.com/posts/' + postID)
-        .then((resp) => {
+        // Step 2: Request posts from WordPress.com
+        axios.get('https://public-api.wordpress.com/rest/v1.1/sites/stearns.wordpress.com/posts/')
+             .then(( resp ) => {
 
-          this.selectedPost = resp.body
-        })
+                  // debugger
+
+                 // Step 3: Save posts array to our data object.
+                 this.posts = resp.data.posts
+             })
+    },
+    methods: {
+
+        // Step 4: Click on a post title to activate this function.
+        selectPost( postID ) {
+            axios.get('https://public-api.wordpress.com/rest/v1.1/sites/stearns.wordpress.com/posts/' + postID)
+                 .then(( resp ) => {
+
+                    // debugger
+
+                     // Step 5: Save our data to the selectedPost property to update the view.
+                     this.selectedPost = resp.data
+                 })
+        }
     }
-  }
 })
